a4531e170b1478cc706bca7e8dbae1c5a8d91c66
joaoduarte01 <joao_duarte3@estudante.sesisenai.org.br>
2025-08-14 09:17:04 -0300

    correções

--- db.sql
+++ db.sql
@@ -8,7 +8,20 @@ CREATE TABLE usuarios (
     senha_hash VARCHAR (255) NOT NULL,
     telefone VARCHAR (20),
     data_contratacao DATE NOT NULL
-)
+);
+
 CREATE TABLE produtos (
-    id_produto INT AUTO_INCREMENT PRIMARY KEY
+    id_produto INT AUTO_INCREMENT PRIMARY KEY, 
+    nome VARCHAR(100) NOT NULL,
+    descricao TEXT NOT FULL, 
+    preco DECIMAL (10,2) NOT NULL,
+    quantidade_estoque INT NOT NULL,
+
+    id_usuario INT NOT NULL,
+    FOREIGN KEY (id_usuario) REFERENCES usuarios (id_usuario)
+
+);
+
+CREATE TABLE clientes(
+    id_clientes 
 )
